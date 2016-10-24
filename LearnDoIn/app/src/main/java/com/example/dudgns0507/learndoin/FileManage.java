915ba0389bfaa2f2e19225b9fa5c6ef42cc563c6
DESKTOP-5VETL7L\sonbi <sonbill971@gmail.com>
2016-10-24 19:40:50 +0900

    FileManage_Created

---
+++ LearnDoIn/app/src/main/java/com/example/dudgns0507/learndoin/FileManage.java
@@ -0,0 +1,28 @@
+package com.example.dudgns0507.learndoin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created by sonbi on 2016-10-24.
+ */
+public class FileManage {
+    public FileManage()
+    {
+
+    }
+    public Map<String,String> parse(String data, String replace)
+    {
+        Map<String,String> wordData = new HashMap<String,String>();
+        StringTokenizer tokenizer = new StringTokenizer(data,replace);
+        for(int i=0;i<=tokenizer.countTokens();++i){
+            String key = tokenizer.nextToken();
+            String value = tokenizer.nextToken();
+            wordData.put(key,value);
+        }
+        return wordData;
+    }
+}
+
+
